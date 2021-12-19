f04fddc09f1f93d4b57b9287139bce05b380f472
h5wawaji <34447486+h5wawaji@users.noreply.github.com>
2021-12-19 13:42:21 +0800

    Update 1.php
    改为 png 清晰点

--- 1.php
+++ 1.php
@@ -1,6 +1,10 @@
 <?php
+# 测试pdf转png
+# 前提
+## 安装 php 扩展 imagemagick 可以 在bt 安装 
+## yum install -y ghostscript 
 $im = new imagick('1.pdf[0]');
-$im->setImageFormat('jpg');
-header('Content-Type: image/jpeg');
+$im->setImageFormat('png');
+header('Content-Type: image/png');// jpg 很差
 echo $im;
 ?>
